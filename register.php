3d16f3cd481e1acc15ebb05f6c0707f6ccb7ce60
José Morais <jose.morais.96@hotmail.com>
2018-10-25 20:34:01 +0000

    first commit

---
+++ register.php
@@ -0,0 +1,143 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname="segurancaWeb";
+
+
+    if(isset($_POST['pass'])){
+        
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            //echo "Connected successfully";
+        }
+        catch(PDOException $e)
+        {
+            echo "Connection failed: " . $e->getMessage();
+        }   
+        
+        if(isset($conn))
+        {
+            $name = $_POST['name']; 
+            $user = $_POST['username']; 
+            $pass = $_POST['pass']; 
+            $checkPass = $_POST['pass']; 
+            if ($pass == $checkPass){
+                $sql = "INSERT INTO user (username, password, name) 
+                        VALUES (:username, :password, :name)";
+                        
+                       // $data = mysql_query ($sql)or die(mysql_error()); 
+                $stm=$conn->prepare($sql);
+                
+                $password = $_POST['pass'];
+                $hashed_password =  crypt ($password ,"salt" );
+                
+                $login = array ('username' => $user, 'password' => $hashed_password, 'name' => $name);
+                
+                $stm->execute($login);
+                
+                header("location:home.php");
+                }
+            }
+    }
+?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Login V3</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="image/png" href="images/logo/icone.png"/>
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100" style="background-image: url('images/image02.jpg');">
+			<div class="wrap-login100grey">
+				<form class="login100-form validate-form" method="post">
+					<span class="login100-form-logo">
+						<img src="images/logo/logo.png">
+					</span>
+
+					<span class="login100-form-title p-b-34 p-t-27">
+						Registar
+					</span>
+					
+					<div class="wrap-input100 validate-input" data-validate = "Enter name">
+						<input class="input100" type="text" name="name" placeholder="Nome">
+						<span class="focus-input100" data-placeholder="&#xf207;"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate = "Enter username">
+						<input class="input100" type="text" name="username" placeholder="Username">
+						<span class="focus-input100" data-placeholder="&#xf207;"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate="Enter password">
+						<input class="input100" type="password" name="pass" placeholder="Password">
+						<span class="focus-input100" data-placeholder="&#xf191;"></span>
+					</div>
+					
+					<div class="wrap-input100 validate-input" data-validate="Confirm password">
+						<input class="input100" type="password" name="pass" placeholder="Confirmar password">
+						<span class="focus-input100" data-placeholder="&#xf191;"></span>
+					</div>
+<!--
+					<div class="contact100-form-checkbox">
+						<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
+						<label class="label-checkbox100" for="ckb1">
+							Remember me
+						</label>
+					</div>
+-->
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							Registar
+						</button>
+					</div>
+<!--
+					<div class="text-center p-t-90">
+						<a class="txt1" href="register.php">
+							Registar
+						</a>
+					</div>
+-->
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
+</html>
